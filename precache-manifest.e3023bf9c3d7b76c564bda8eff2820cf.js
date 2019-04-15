8d6a1c693ddbd6ec4160c4b1bd891f132202e73b
Arif <44020819+azimarif@users.noreply.github.com>
2019-04-15 18:22:46 +0530

    Updates

---
+++ precache-manifest.e3023bf9c3d7b76c564bda8eff2820cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d4cc52b58bd19065a8a",
+    "url": "/solitaire/static/js/runtime~main.1eee08ec.js"
+  },
+  {
+    "revision": "01ea16c446f0cd1ee857",
+    "url": "/solitaire/static/js/main.25d7cc60.chunk.js"
+  },
+  {
+    "revision": "3d891ddabec18fb959d9",
+    "url": "/solitaire/static/js/2.6b853315.chunk.js"
+  },
+  {
+    "revision": "01ea16c446f0cd1ee857",
+    "url": "/solitaire/static/css/main.e933a848.chunk.css"
+  },
+  {
+    "revision": "d79751e99adbb4584201f0aa68a173ba",
+    "url": "/solitaire/index.html"
+  }
+];
